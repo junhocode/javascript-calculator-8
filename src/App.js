d60d0ccd363ed4c314967abd236f5e03b48583d5
진준호 <161407999+junhocode@users.noreply.github.com>
2025-10-19 15:57:52 +0900

    feat: Implement application execution logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+import MESSAGES from './messages.js';
+import calculator from './calculator.js';
+import inputParser from './inputParser.js';
+
 class App {
-  async run() {}
+ async run(){
+  try {
+    const input = await Console.readLineAsync(MESSAGES.START_MESSAGE)
+    const numbers = inputParser(input);
+    const sum = calculator(numbers);
+    Console.print(MESSAGES.RESULT_MESSAGE(sum));
+  } catch (error) {
+    Console.print(MESSAGES.ERROR_MESSAGE(error));
+    throw error;
+  }
+ }
 }
 
-export default App;
+export default App; 
