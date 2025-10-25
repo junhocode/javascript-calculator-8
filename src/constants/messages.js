ae35b0fa101089a04a29cd4f400c00a4acbd31f4
진준호 <theworstvalentine@gmail.com>
2025-10-25 13:49:11 +0900

    style: Separate Ui Messages and Error Messages into different constants

---
+++ src/constants/messages.js
@@ -0,0 +1,10 @@
+export const UI_MESSAGES = {
+  START: '덧셈할 문자열을 입력해 주세요.\n',
+  RESULT: (sum) => `결과 : ${sum}`,
+};
+
+export const ERROR_MESSAGES = {
+  INVALID_DELIMITER: '[ERROR] 커스텀 구분자는 숫자나 공백이 될 수 없습니다.',
+  INVALID_INPUT: '[ERROR] 잘못된 입력입니다. 음수 또는 숫자가 아닌 값이 포함되어 있습니다.',
+  WRAPPED: (error) => `[ERROR] ${error.message}`,
+};
