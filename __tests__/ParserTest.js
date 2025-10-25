e78230b5a4f26552c289e91187f7ac52f92bb564
진준호 <theworstvalentine@gmail.com>
2025-10-25 14:55:17 +0900

    refactor: Refactor ParserTest using giventhenwhen format

--- __tests__/ParserTest.js
+++ __tests__/ParserTest.js
@@ -1,75 +1,52 @@
-import inputParser from "../src/inputParser.js";
-import MESSAGES from "../src/messages.js";
+import parser from "../src/service/models/parser.js";
+import { ERROR_MESSAGES } from "../src/constants/messages.js";
 
-describe("inputParser", () => {
-  describe("기본 구분자", () => {
-    test("쉼표 구분자 테스트", () => {
-      const input = "1,2,3";
-      expect(inputParser(input)).toEqual([1, 2, 3]);
-    });
+describe("parser", () => {
+  describe("GIVEN: 기본 구분자", () => {
+    it("THEN: 구분자 기준 나뉜 숫자 배열 반환", () => {
+      const input1 = "1,2,3";
+      const input2 = "1:2:3";
+      const input3 = "1,2:3";
+      const input4 = "5";
 
-    test("콜론 구분자 테스트", () => {
-      const input = "1:2:3";
-      expect(inputParser(input)).toEqual([1, 2, 3]);
-    });
-
-    test("쉼표, 콜론 구분자 테스트", () => {
-      const input = "1,2:3";
-      expect(inputParser(input)).toEqual([1, 2, 3]);
-    });
-
-    test("단일 숫자 테스트", () => {
-      const input = "5";
-      expect(inputParser(input)).toEqual([5]);
+      expect(parser(input1)).toEqual([1, 2, 3]);
+      expect(parser(input2)).toEqual([1, 2, 3]);
+      expect(parser(input3)).toEqual([1, 2, 3]);
+      expect(parser(input4)).toEqual([5]);
     });
   });
 
-  describe("커스텀 구분자", () => {
-    test("커스텀 구분자 테스트", () => {
-      const input = "//;\\n1;2;3";
-      expect(inputParser(input)).toEqual([1, 2, 3]);
+  describe("GIVEN: 커스텀 구분자", () => {
+    it("THEN: 커스텀 구분자, 기본 구분자 모두 사용해 나뉜 숫자 배열 반환", () => {
+      const inputWithCustomDelimiter = "//;\\n1;2;3";
+      const inputWithMixedDelimiters = "//;\\n1;2,3:4";
+
+      expect(parser(inputWithCustomDelimiter)).toEqual([1, 2, 3]);
+      expect(parser(inputWithMixedDelimiters)).toEqual([1, 2, 3, 4]);
     });
 
-    test("커스텀, 기본 구분자 테스트", () => {
-      const input = "//;\\n1;2,3:4";
-      expect(inputParser(input)).toEqual([1, 2, 3, 4]);
-    });
-
-    test("숫자 없는 커스텀 구분자 테스트", () => {
-      const input = "//;\\n";
-      expect(inputParser(input)).toEqual([0]);
-    });
-  });
-
-  describe("예외 처리", () => {
-    test("입력값이 null 혹은 undefined인 경우", () => {
-      expect(inputParser(null)).toEqual([]);
-      expect(inputParser(undefined)).toEqual([]);
-    });
-
-    test("빈 입력값인 경우 ", () => {
-      expect(inputParser("")).toEqual([]);
-      expect(inputParser("  ")).toEqual([]);
-    });
-
-    test("커스텀 구분자가 숫자인 경우", () => {
-      const input = "//1\\n1,2,3";
-      expect(() => inputParser(input)).toThrow(MESSAGES.INVALID_DELIMITER_ERROR);
-    });
-
-    test("커스텀 구분자가 공백인 경우", () => {
-      const input = "// \\n1 2,3";
-      expect(() => inputParser(input)).toThrow(MESSAGES.INVALID_DELIMITER_ERROR);
-    });
-  });
-
-  describe("반환값 테스트", () => {
-    test("반환된 배열의 모든 요소는 숫자 타입이어야 한다.", () => {
-      const input = "1,2,3";
-      const result = inputParser(input);
-      result.forEach((item) => {
-        expect(typeof item).toBe("number");
+    describe("GIVEN: 숫자가 없는 문자열", () => {
+      it("THEN: 0 배열 반환", () => {
+        const input = "//;\\n";
+        expect(parser(input)).toEqual([0]);
       });
+    });
+  });
+
+  describe("GIVEN: 예외", () => {
+    it("THEN: 빈 배열 반환", () => {
+      expect(parser(null)).toEqual([]);
+      expect(parser(undefined)).toEqual([]);
+      expect(parser("")).toEqual([]);
+      expect(parser("  ")).toEqual([]);
+    });
+
+    it("THEN: 에러를 던진다", () => {
+      const inputWithNumberDelimiter = "//1\\n1,2,3";
+      const inputWithWhitespaceDelimiter = "// \\n1 2,3";
+
+      expect(() => parser(inputWithNumberDelimiter)).toThrow(ERROR_MESSAGES.INVALID_DELIMITER);
+      expect(() => parser(inputWithWhitespaceDelimiter)).toThrow(ERROR_MESSAGES.INVALID_DELIMITER);
     });
   });
 });
